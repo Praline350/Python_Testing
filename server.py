fa4e8aeef39324d6a28e98efce61e0c57766c9ec
edwin thierry <edwin.thierry350@gmail.com>
2024-08-13 08:34:59 +0200

    Adjustement of routes

--- server.py
+++ server.py
@@ -1,66 +1,71 @@
 import json
 import os
 from dotenv import load_dotenv
-from flask import Flask,render_template,request,redirect,flash,url_for
+from flask import Flask, render_template, request, redirect, flash, url_for
 from tools.tools import Utils, DataBase
-
 
 load_dotenv()
 
+class Server:
+    def __init__(self):
+        self.app = Flask(__name__)
+        self.app.secret_key = 'something_special'
+        self.data_base = DataBase()
+        self.utils = Utils()
+        self.competitions = self.data_base.load_competitions()
+        self.data_base.check_competitions_status(self.competitions)
+        self.clubs = self.data_base.load_clubs()
 
-data_base = DataBase()
-utils = Utils()
+        # Configuration des routes
+        self.app.add_url_rule('/', 'index', self.index)
+        self.app.add_url_rule('/showSummary', 'showSummary', self.showSummary, methods=['POST'])
+        self.app.add_url_rule('/book/<competition>/<club>', 'book', self.book)
+        self.app.add_url_rule('/purchasePlaces', 'purchasePlaces', self.purchasePlaces, methods=['POST'])
+        self.app.add_url_rule('/PointsBoard', 'pointsBoard', self.pointsBoard, methods=['GET'])
+        self.app.add_url_rule('/logout', 'logout', self.logout)
 
-app = Flask(__name__)
-app.secret_key = 'something_special'
+    def index(self):
+        return render_template('index.html')
 
-competitions = data_base.load_competitions()
-clubs = data_base.load_clubs()
+    def showSummary(self):
+        email = request.form['email']
+        selected_club = self.utils.find_club_by_email(email, self.clubs)
+        if selected_club is None:
+            flash("Email wrong")
+            return redirect(url_for('index'))
+        return render_template('welcome.html', club=selected_club, competitions=self.competitions)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+    def book(self, competition, club):
+        foundClub = [c for c in self.clubs if c['name'] == club][0]
+        foundCompetition = [c for c in self.competitions if c['name'] == competition][0]
+        if foundClub and foundCompetition:
+            return render_template('booking.html', club=foundClub, competition=foundCompetition)
+        else:
+            flash("Something went wrong-please try again")
+            return render_template('welcome.html', club=club, competitions=self.competitions)
 
-@app.route('/showSummary', methods=['POST'])
-def showSummary():
-    email = request.form['email']
-    selected_club = utils.find_club_by_email(email, clubs)
-    if selected_club is None:
-        flash("Email wrong")
+    def purchasePlaces(self):
+        competition = [c for c in self.competitions if c['name'] == request.form['competition']][0]
+        club = [c for c in self.clubs if c['name'] == request.form['club']][0]
+        placesRequired = int(request.form['places'])
+        validation = self.utils.club_add_places(club, competition, placesRequired)
+        if not validation:
+            flash("Exceeding the authorized limit or club points")
+        else:
+            message = self.utils.point_ajustement(club, competition, placesRequired)
+            flash(message)
+        return render_template('welcome.html', club=club, competitions=self.competitions)
+
+    def pointsBoard(self):
+        return render_template('board.html', clubs=self.clubs, competitions=self.competitions)
+
+    def logout(self):
         return redirect(url_for('index'))
-    return render_template('welcome.html', club=selected_club, competitions=competitions)
 
+    def run(self, host='0.0.0.0', port=5000, debug=True):
+        self.app.run(host=host, port=port, debug=debug)
 
-@app.route('/book/<competition>/<club>')
-def book(competition,club):
-    foundClub = [c for c in clubs if c['name'] == club][0]
-    foundCompetition = [c for c in competitions if c['name'] == competition][0]
-    if foundClub and foundCompetition:
-        return render_template('booking.html',club=foundClub,competition=foundCompetition)
-    else:
-        flash("Something went wrong-please try again")
-        return render_template('welcome.html', club=club, competitions=competitions)
-
-
-@app.route('/purchasePlaces',methods=['POST'])
-def purchasePlaces():
-    competition = [c for c in competitions if c['name'] == request.form['competition']][0]
-    club = [c for c in clubs if c['name'] == request.form['club']][0]
-    placesRequired = int(request.form['places'])
-    validation = utils.club_add_places(club, competition, placesRequired)
-    if not validation:
-        flash("You cannot reserved more than 12 place per competition")
-    else:
-        message = utils.point_ajustement(club, competition, placesRequired)
-        flash(message)
-    return render_template('welcome.html', club=club, competitions=competitions)
-
-
-
-@app.route('/PointsBoard', methods=['GET'])
-def pointsBoard():
-    return render_template('board.html', clubs=clubs, competitions=competitions)
-
-@app.route('/logout')
-def logout():
-    return redirect(url_for('index'))
+# Exécution du serveur
+if __name__ == '__main__':
+    server = Server()
+    server.run()
