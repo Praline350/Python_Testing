9cb046cde371f3744ab4815982590bd3a43a3aef
edwin thierry <edwin.thierry350@gmail.com>
2024-08-27 11:11:35 +0200

    QA v0.9.1

--- tests/tests_integration/test_integration.py
+++ tests/tests_integration/test_integration.py
@@ -14,15 +14,15 @@ def server():
     thread = Thread(target=server.run, kwargs={'host': '127.0.0.1', 'port': 5000, 'debug': False})
     thread.daemon = True
     thread.start()
-    time.sleep(1)  # Attendre un moment pour s'assurer que le serveur est bien lancé
+    time.sleep(1)
     yield server
-    # Arrêter le serveur après les tests (si nécessaire)
+    # Arrêter le serveur après les tests 
     
 @pytest.fixture(scope="module")
 def browser():
     service = FirefoxService(executable_path=GeckoDriverManager().install())
     driver = webdriver.Firefox(service=service)
-    driver.implicitly_wait(10)  # Attente implicite pour les éléments
+    driver.implicitly_wait(5)  
 
     yield driver
 
@@ -30,7 +30,8 @@ def browser():
 
 def test_index_page(server, browser):
     browser.get('http://127.0.0.1:5000/')
-    assert "Welcome to the GUDLFT Registration Portal!" in browser.page_source  # Assure-toi que c'est le bon titre de ta page
+    time.sleep(8)
+    assert "Welcome to the GUDLFT Registration Portal!" in browser.page_source 
 
 def test_full_functionality_flow(server, browser):
     # Étape 1: Connexion
@@ -38,17 +39,19 @@ def test_full_functionality_flow(server, browser):
     
     # Entrer l'email et soumettre le formulaire
     email_input = browser.find_element(By.NAME, 'email')
-    email_input.send_keys('kate@shelifts.co.uk')  # Utilise un email valide dans ta base de données
+    email_input.send_keys('kate@shelifts.co.uk') 
+    time.sleep(8) 
     
     submit_button = browser.find_element(By.XPATH, "//button[text()='Enter']")
     submit_button.click()
     
-    # Vérifier que la connexion a réussi
+
     assert "Welcome, kate@shelifts.co.uk" in browser.page_source
     
     # Étape 2: Réservation (Booking)
     # Cliquer sur le lien pour réserver une place
     booking_link = browser.find_element(By.LINK_TEXT, f"Book Places")
+    time.sleep(8)
     booking_link.click()
     
     # Remplir le formulaire de réservation
@@ -56,6 +59,7 @@ def test_full_functionality_flow(server, browser):
     places_input.send_keys('1')
     
     book_button = browser.find_element(By.XPATH, "//button[text()='Book']")
+    time.sleep(8)
     book_button.click()
     
     # Vérifier que la réservation a réussi
@@ -63,7 +67,9 @@ def test_full_functionality_flow(server, browser):
     
     # Étape 3: Accéder au tableau des points (Points Board)
     points_board_link = browser.find_element(By.LINK_TEXT, "Points Board")
+    time.sleep(8)
     points_board_link.click()
+    time.sleep(5)
     
     # Vérifier que le tableau des points est affiché
     assert "Points Board" in browser.page_source
